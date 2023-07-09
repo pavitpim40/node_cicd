6ed67f0d9c63f23bb07be2ad0c8e2444b48662d1
Pavit Pimchanagul <pavitpim@gmail.com>
2023-07-10 01:37:37 +0700

    update node

--- app.js
+++ app.js
@@ -1,17 +1,13 @@
 const express = require('express');
-const add = require('./add');
+const cors = require('cors');
 // const cors = require('cors');
 
 const app = express();
-// app.use(cors());
+app.use(cors());
 
 app.get('/', (req, res) => {
     add(1, 2);
     res.send(`<h1>Hello world from Express with CICD</h1>`);
-});
-
-app.listen(8000, () => {
-    console.log('Server is running on port 8000');
 });
 
 // docker buildx build --platform linux/amd64 -t pavitpim40/simple-express-api:1.0.0 --push .
