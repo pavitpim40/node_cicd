f2833e510c29b217444bd066936d869b1f1204e1
Pavit Pimchanagul <pavitpim@gmail.com>
2023-07-10 16:22:54 +0700

    update listen

--- app.js
+++ app.js
@@ -10,4 +10,8 @@ app.get('/', (req, res) => {
     res.send(`<h1>Hello world from Express with CICD</h1>`);
 });
 
+app.listen(8000, () => {
+    console.log('Server is listening on port 8000');
+});
+
 // docker buildx build --platform linux/amd64 -t pavitpim40/simple-express-api:1.0.0 --push .
